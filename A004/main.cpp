81086944133847fcd3f3eb74e15b630c65f7fe28
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2024-11-04 21:34:57 -0500

    Comments

---
+++ A004/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//Entry point main function
+int main(int argc, char **argv){
+
+    // One line comment
+
+    /*
+        Multi-line block comment
+        Another line
+        Yet, Another line!
+    */
+    //Print out some tezt
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Number 1 : " << std::endl;
+    std::cout << "Number 2 : " << std::endl;
+    std::cout << argc << std::endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -5,3 +5,10 @@ installed
 sudo apt-get deb gcc+-10
 
 g++-10 -std=c++20 main.cpp -o main
+
+check point 
+2024.11.01 - 1
+
+A003 - First CCP program
+
+1:55 - A004 comments in CPP
