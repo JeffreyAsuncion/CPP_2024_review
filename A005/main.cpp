0741744b19fa27969dde547813d6b8d8331a227a
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2024-11-06 20:39:48 -0500

    compile time errors, runtime errors, and warnings

---
+++ A005/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// Types of Errors
+int main(int argc, char **argv){
+
+    /*
+        Compile Time Errors
+        Runtime Errors
+        Warnings
+    */
+
+    // Compile time error example - missing ;
+    /*
+    std::cout << "Hello World!" << std::endl
+    */
+    
+    // Runtime Error Example - will cause a program to crash
+    /*
+    int value = 7/0;
+    std::cout << "value : " << value << std::endl;
+    */
+
+    // Warning example - Divide by Zero
+    
+
+    return 0;
+}
