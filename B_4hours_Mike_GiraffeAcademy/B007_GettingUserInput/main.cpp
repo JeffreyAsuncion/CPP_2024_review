5514c92a35a5efa664f05ff11543301f5c3d0ca2
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2024-11-08 21:18:28 -0500

    getting user input, cin, getline()

---
+++ B_4hours_Mike_GiraffeAcademy/B007_GettingUserInput/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    
+    int age;
+    char letter;
+    string name;
+    /*
+    cout << "Enter your age: " << endl;
+    cin >> age;
+
+    cout << "You are " << age << " years old." << endl;
+
+    cout << "Enter a Letter: " << endl;
+    cin >> letter;
+
+    cout << letter << " is a fine looking letter." << endl;
+    */
+
+
+    cout << "Enter your name: " << endl;
+    getline(cin, name);
+    cout << name << " is a cool name." << endl; 
+
+    // 1:05
+
+    return 0;
+}
