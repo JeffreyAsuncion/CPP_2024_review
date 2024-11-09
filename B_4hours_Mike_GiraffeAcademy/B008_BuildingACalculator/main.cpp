fc1fc97e244a5921f347188a016b0c06cbbfadeb
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2024-11-08 21:27:00 -0500

    Building a simple adding calculator

---
+++ B_4hours_Mike_GiraffeAcademy/B008_BuildingACalculator/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // basic calculator to add to numbers
+
+    double num1, num2;
+    
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "The sum is: " << num1 + num2 << endl;
+        
+
+    return 0;
+}
